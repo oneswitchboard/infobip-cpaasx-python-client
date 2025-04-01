80587009ae006d5ae4496ed209bd41a129900759
st585 <sanjana@oneswitchboard.com>
2025-04-01 17:50:10 -0400

    removing print

--- infobip_cpaasx/api_client.py
+++ infobip_cpaasx/api_client.py
@@ -224,7 +224,6 @@ class ApiClient(object):
             url += "?" + url_query
 
         try:
-            print(body)
             # perform request and return response
             response_data = self.request(
                 method,
